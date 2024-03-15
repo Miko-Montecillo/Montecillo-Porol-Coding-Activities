4c2f1bec12b882a8bccb47f5d371a1dbb5262e46
Ayekal <115404368+Ayekal@users.noreply.github.com>
2024-03-15 10:51:55 +0800

    Update CSCC13Proj.java

--- Midterms/CSCC13Proj.java
+++ Midterms/CSCC13Proj.java
@@ -6,19 +6,42 @@ public class CSCC13Proj {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of test case");
+        // test case indicates the rounds
+        System.out.println("Enter number of test cases:");
         int testCases = sc.nextInt();
 
-        System.out.println("Enter Name, health and Power in this Format 'NAME H P");
-for(int i = 0; i < testCases; i++)
-{
-        String name = sc.next();
-        int health = sc.nextInt();
-        int power = sc.nextInt();
+        System.out.println("Enter Name, Health, and Power for combatant 1:");
+        for (int testCase = 0; testCase < testCases; testCase++) {
+            // name, health and power of the pirate
+            String name1 = sc.next();
+            int health1 = sc.nextInt();
+            int power1 = sc.nextInt();
+            // name, health and power of the ninja
+            String name2 = sc.next();
+            int health2 = sc.nextInt();
+            int power2 = sc.nextInt();
 
-        // System.out.println(name + " " + health + " " + power);
-}
+            // Simulate the combat
+            int winnerHealth = simulateCombat(health1, power1, health2, power2);
 
+            // Determine the winner
+            String winnerName = (winnerHealth == health1) ? name1 : name2;
 
+            // Output the result
+            System.out.println(winnerName + " " + winnerHealth);
+        }
+        sc.close();
     }
-} 
+
+    // Function to simulate the combat and return the remaining health of the winner
+    private static int simulateCombat(int health1, int power1, int health2, int power2) {
+        while (health1 > 0 && health2 > 0) {
+            health2 -= power1;
+            if (health2 <= 0) {
+                return health1;
+            }
+            health1 -= power2;
+        }
+        return health2;
+    }
+}
