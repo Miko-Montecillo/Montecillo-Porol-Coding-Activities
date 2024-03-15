9cad2fda68fe57f36283258406f13fa00fe5e224
Ayekal <115404368+Ayekal@users.noreply.github.com>
2024-03-15 09:17:44 +0800

    Add files via upload

---
+++ Midterms/CSCC13Proj.java
@@ -0,0 +1,24 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class CSCC13Proj {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of test case");
+        int testCases = sc.nextInt();
+
+        System.out.println("Enter Name, health and Power in this Format 'NAME H P");
+for(int i = 0; i < testCases; i++)
+{
+        String name = sc.next();
+        int health = sc.nextInt();
+        int power = sc.nextInt();
+
+        // System.out.println(name + " " + health + " " + power);
+}
+
+
+    }
+} 
